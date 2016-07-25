24c151940d6004cc96aeea29fb7ce2936b75cd7e
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-07-25 05:08:25 -0700

    added default seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+from = DateTime.new(2000,1,1).to_f
+to = DateTime.new(2018,12,31).to_f
+hostnames = [ "Nike", "Addidas", "Puma", "Reebok", "Vans", "Fila", "Timberland", "Converse" ]
+org = Apartment::Tenant.current
+
+Org.create( name: "New Relic", subdomain: "newrelic" ) unless Apartment.tenant_names.include?( 'newrelic' )
+Org.create( name: "Old Relic", subdomain: "oldrelic" ) unless Apartment.tenant_names.include?( 'oldrelic' )
+Org.create( name: "Cur Relic", subdomain: "currelic" ) unless Apartment.tenant_names.include?( 'currelic' )
+Org.create( name: "Nex Relic", subdomain: "nexrelic" ) unless Apartment.tenant_names.include?( 'nexrelic' )
+Org.create( name: "Pre Relic", subdomain: "prerelic" ) unless Apartment.tenant_names.include?( 'prerelic' )
+
+500.times do |index|
+  date = Time.at( rand * ( to - from ) + from )
+  rand = Random.rand( hostnames.size )
+  Event.create!( name: org + " " + index.to_s,
+                 host: hostnames[ rand ],
+                 date: date
+                 )
+end
+puts "Seeding #{ org } tenant is done"
+puts
+
