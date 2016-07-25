9333585bab1009202e6c6e5c099dc3cf3414b088
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-07-25 06:25:14 -0700

    event model & specs

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ActiveRecord::Base
+  def self.search_the_last( limit )
+    order( :date ).limit( limit ).reverse_order
+  end
 end
+
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!( :event1 ) { FactoryGirl.create( :event ) }
+  let!( :event2 ) { FactoryGirl.create( :event, date: "2001-01-01 00:00:00" ) }
+  let!( :event3 ) { FactoryGirl.create( :event, host: "Nike", date: "2002-01-01 00:00:00" ) }
+
+  context "happy path", use_transactions: false do
+
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ).size ).to be( 2 ) }
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ) ).to match_array( [ event2, event1 ] ) }
+
+    it{ expect( Event.search_the_last(2) ).to match_array( [ event3, event2 ] ) }
+
+  end
+
+  context "unhappy path", use_transactions: false do
+
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ).size ).to_not be( 1 ) }
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ).size ).to_not be( 3 ) }
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ) ).to_not eq( [ event1, event2 ] ) }
+    it{ expect( Event.search_the_last(2).where( host: "Reebok" ) ).to_not include( event3 ) }
+
+    it{ expect( Event.search_the_last(2) ).to_not eq( [ event2, event3 ] ) }
+    it{ expect( Event.search_the_last(2) ).to_not eq( [ event1, event3 ] ) }
+    it{ expect( Event.search_the_last(2) ).to_not eq( [ event3, event1 ] ) }
+    it{ expect( Event.search_the_last(2) ).to_not eq( [ event3, event3 ] ) }
+
+  end
+
 end
+
